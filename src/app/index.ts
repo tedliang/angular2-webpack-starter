54861876ebce06b64470ff2c2fc4c48d509fd874
Ted Liang <teddyliang@gmail.com>
2016-08-28 21:24:16 +1000

    Merge branch 'master' of https://github.com/AngularClass/angular2-webpack-starter # Conflicts: #	package.json #	src/app/app.routes.ts #	src/app/index.ts

--- src/app/index.ts
+++ src/app/index.ts
@@ -1,24 +1,2 @@
 // App
-export * from './app.component';
-export * from './app.service';
-export * from './app.routes';
-
-import { AppState } from './app.service';
-import { APP_REDUCERS } from './todos/reducers';
-import { APP_ACTIONS } from './todos/actions';
-
-import { provideStore, combineReducers } from '@ngrx/store';
-import { compose } from '@ngrx/core/compose';
-import { localStorageSync } from 'ngrx-store-localstorage';
-
-// Application wide providers
-export const APP_PROVIDERS = [
-  APP_ACTIONS,
-  provideStore(
-    compose(
-      localStorageSync(['todos'], true),
-      combineReducers
-    )(APP_REDUCERS)
-  ),
-  AppState
-];
+export * from './app.module';
